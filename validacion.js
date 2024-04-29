fd5163df05b75a83ba0413b6b04bd5e4307eb95c
Jesus Gomez <gomjesal@gmail.com>
2024-04-28 22:45:19 -0400

    versión final, corregido funcionamiento de envío de correos

--- validacion.js
+++ validacion.js
@@ -78,3 +78,42 @@ document.getElementById('contacto').addEventListener('submit', function (e){
     }
 
 });
+
+const form = document.getElementById('form');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Espere por favor..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Ups, algo salió mal!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
+});
